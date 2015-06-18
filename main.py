d4231fb2e53c315341ed3bf3a422cba158c548db
Mihai Gramada <gramada.mihai@gmail.com>
2015-06-18 14:23:56 +0200

    updated search API in the CLI

--- main.py
+++ main.py
@@ -12,27 +12,28 @@ TEST_ENTITY = "urn:ers:test"
 class Interface(object):
     def __init__(self):
         self.ers = ERS()
-        
+
     def clean_db(self):
         '''
         Reset all the content of the DB
         '''
         self.ers.reset()
-    
+
     # List all the peers
-    
+
     # List the local statements about an entity (groupe them by status)
-    
+
     # Delete all statements about an entity
-    
+
     # List the peers that have statements about an entity
-    def search_for_entity(self, uri):
+    def search_for_entity(self, prop, val=None):
         table = self._table(["Peer name"])
-        # list_of_peers = self.ers.search_by_uri(uri)
-        print table
+        list_of_peers = self.ers.search(prop,val)
+        print list_of_peers
+        #print table
 
     # Cache remote documents
-        
+
     # Add a public|private p/o to an entity
     def add_statement(self, uri, predicate, value, private=False):
         '''
@@ -43,22 +44,22 @@ class Interface(object):
         entity.add(predicate, value, private)
         self.ers.persist_entity(entity)
         print "Added <%s, %s, %s> (private=%s)" % (uri, predicate, value, private)
-        
+
     # Set a public|private p/o from an entity
     def set_statement(self, uri, predicate, value, private=False):
         entity = self.ers.get(uri)
         entity.set(predicate, value, private)
         self.ers.persist_entity(entity)
         print "Set <%s, %s, %s> (private=%s)" % (uri, predicate, value, private)
-        
+
     # Delete a public|private p/o from an entity
-    
+
     def delete_statement(self, uri, predicate, value=None):
         entity = self.ers.get(uri)
         entity.delete(predicate, value)
         self.ers.persist_entity(entity)
         print "Deleted <%s, %s, %s>" % (uri, predicate, value)
-    
+
     # Show internal synchronisation rules
 
     def show_status(self):
@@ -75,11 +76,11 @@ class Interface(object):
         Helper function to initialise pretty table
         '''
         table = PrettyTable(columns)
-        for column in columns: 
+        for column in columns:
             table.align[column] = 'l'
         return table
 
-    
+
     def show_entity(self, uri):
         entity = self.ers.get(uri)
         table = self._table(["Predicate", "Value", "Scope"])
@@ -87,10 +88,10 @@ class Interface(object):
             (p, o, scope) = statement
             table.add_row([p, o, scope])
         print table
-    
+
     def import_entity(self, entity_uri):
         entities = {}
-        
+
         # Load
         g = Graph()
         g.parse(entity_uri)
@@ -104,32 +105,37 @@ class Interface(object):
             entity.add(p, o)
         for entity in entities.itervalues():
             self.ers.persist_entity(entity)
-        
+
         # Display
         for entity_uri in entities.iterkeys():
             print "\n=> %s " % entity_uri
             self.show_entity(entity_uri)
-            
+
 if __name__ == '__main__':
     interface = Interface()
     interface.clean_db()
-    # print "# Status"
-    # interface.show_status()
-    # print "# Search for %s" % TEST_ENTITY
-    # interface.search_for_entity(TEST_ENTITY)
-    # print "# Adding statements to %s" % TEST_ENTITY
-    # interface.add_statement(TEST_ENTITY, "rdf:type", "foaf:Agent")
-    # interface.add_statement(TEST_ENTITY, "rdf:type", "foaf:Person", True)
-    # interface.show_entity(TEST_ENTITY)
-    # print "# Setting statements for %s" % TEST_ENTITY
-    # interface.set_statement(TEST_ENTITY, "rdf:type", "foaf:Agent")
-    # interface.show_entity(TEST_ENTITY)
-    # interface.set_statement(TEST_ENTITY, "rdf:type", "foaf:SecretAgent", True)
-    # interface.show_entity(TEST_ENTITY)
-    # print "# Delete statements for %s" % TEST_ENTITY
-    # interface.delete_statement(TEST_ENTITY, "rdf:type", "foaf:Agent")
-    # interface.show_entity(TEST_ENTITY)
-    # interface.delete_statement(TEST_ENTITY, "rdf:type")
-    # interface.show_entity(TEST_ENTITY)
+    #print "# Status"
+    #interface.show_status()
+    print "# Adding statements to %s" % TEST_ENTITY
+    interface.add_statement(TEST_ENTITY, "rdf:type", "foaf:Agent")
+    #interface.add_statement(TEST_ENTITY, "rdf:type", "foaf:DoubleAgent")
+    interface.add_statement(TEST_ENTITY, "rdf:type", "foaf:Person", True)
+    interface.show_entity(TEST_ENTITY)
+    print "# Setting statements for %s" % TEST_ENTITY
+
+    #print "# Search for %s" % TEST_ENTITY
+    q_pred = "rdf:type"
+    q_value = "foaf:Agent"
+    print "# Search for entities which have %s as a predicate and %s as value" % (q_pred, q_value)
+    interface.search_for_entity("rdf:type", "foaf:Agent")
+    interface.set_statement(TEST_ENTITY, "rdf:type", "foaf:Agent")
+    interface.show_entity(TEST_ENTITY)
+    interface.set_statement(TEST_ENTITY, "rdf:type", "foaf:SecretAgent", True)
+    interface.show_entity(TEST_ENTITY)
+    print "# Delete statements for %s" % TEST_ENTITY
+    interface.delete_statement(TEST_ENTITY, "rdf:type", "foaf:Agent")
+    interface.show_entity(TEST_ENTITY)
+    interface.delete_statement(TEST_ENTITY, "rdf:type")
+    interface.show_entity(TEST_ENTITY)
     interface.import_entity("http://people.csail.mit.edu/lkagal/foaf.rdf")
-    
+
